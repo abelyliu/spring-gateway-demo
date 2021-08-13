d3081e869de49fb59a08f2f8093f549fceed73a9
刘青松 <qingsong.liu@crgecent.com>
2021-08-13 14:40:21 +0800

    modify request params

--- backend/src/main/java/com/example/backend/Index.java
+++ backend/src/main/java/com/example/backend/Index.java
@@ -9,8 +9,41 @@ import java.util.Map;
 
 @RestController
 public class Index {
+
+    static class RequestBodyDto {
+        private String name;
+        private Integer age;
+        private String secretIdentity;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public String getSecretIdentity() {
+            return secretIdentity;
+        }
+
+        public void setSecretIdentity(String secretIdentity) {
+            this.secretIdentity = secretIdentity;
+        }
+    }
+
     @RequestMapping("/test")
-    public String index(@RequestBody Map<String, String> params, HttpServletRequest request) {
+    public String index(@RequestBody RequestBodyDto bodyDto, HttpServletRequest request) {
+//        System.out.println("hello");
+//        System.out.println(bodyDto);
         return "hello";
     }
 }
